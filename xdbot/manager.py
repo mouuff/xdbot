0d3e637688143230ee0830bb13b5a1b68f981d40
Arnaud Aliès <arnaudalies.py@gmail.com>
2018-10-06 11:51:37 +0200

    threaded watcher

--- xdbot/manager.py
+++ xdbot/manager.py
@@ -1,20 +1,19 @@
 import time
-from .watchers import WatcherFindAndAccept, WatcherBanChampion
+from .watchers import WatcherFindAndAccept, WatcherBanChampion, WatcherLockBanChampion
 
 
 class Manager:
-    def __init__(self, interval=0.1):
+    def __init__(self, interval=0.05):
         self._running = False
         self._interval = interval
+        watcher_args = (self, interval)
         self._watchers = [
-            WatcherFindAndAccept(self),
-            WatcherBanChampion(self),
+            WatcherFindAndAccept(*watcher_args),
+            WatcherBanChampion(*watcher_args),
+            WatcherLockBanChampion(*watcher_args),
         ]
 
     def mainloop(self):
-        assert not self._running
-        self._running = True
-        while self._running:
-            for watcher in self._watchers:
-                watcher.update()
-            time.sleep(self._interval)
+        for watcher in self._watchers:
+            watcher.start()
+        input()  # thread are running, press to exit
--- xdbot/watchers.py
+++ xdbot/watchers.py
@@ -1,16 +1,31 @@
 from abc import ABC, abstractmethod
+import time
 import pyautogui as auto
+import threading
 from . import misc
 from .constants import Constants
 
 
 class WatcherBase(ABC):
-    def __init__(self, manager):
+    def __init__(self, manager, interval):
         self.manager = manager
+        self._running = False
+        self._interval = interval
+        self._thread = threading.Thread(target=self._mainloop)
+        self._thread.daemon = True
+
+    def _mainloop(self):
+        self._running = True
+        while self._running:
+            self._update()
+            time.sleep(self._interval)
 
     @abstractmethod
-    def update(self):
+    def _update(self):
         pass
+
+    def start(self):
+        self._thread.start()
 
 
 class WatcherFindAndClickBase(WatcherBase):
@@ -19,27 +34,32 @@ class WatcherFindAndClickBase(WatcherBase):
     def _get_res_to_find(self):
         pass
 
-    def click(self, x, y):
+    def _found_res(self, x, y):
         auto.click(x, y)
 
-    def update(self):
+    def _update(self):
         pos = misc.locate_on_screen(self._get_res_to_find())
         if pos is not None:
             x, y = pos
-            self.click(x, y)
+            self._found_res(x, y)
+
+
+class WatcherLockBanChampion(WatcherFindAndClickBase):
+    def _get_res_to_find(self):
+        return Constants.RES_LOCK_PICK
+
+
+class WatcherFindAndAccept(WatcherFindAndClickBase):
+    def _get_res_to_find(self):
+        return Constants.RES_ACCEPT_GAME
 
 
 class WatcherBanChampion(WatcherFindAndClickBase):
     def _get_res_to_find(self):
         return Constants.RES_FIND_BAN
 
-    def click(self, x, y):
+    def _found_res(self, x, y):
         ban = misc.locate_on_screen(Constants.RES_TITLE_BAN)
         if ban is not None:
-            super().click(x, y)
+            super()._found_res(x, y)  # click on find ban bar
             auto.typewrite("morgana")  # test wip
-
-
-class WatcherFindAndAccept(WatcherFindAndClickBase):
-    def _get_res_to_find(self):
-        return Constants.RES_ACCEPT_GAME
