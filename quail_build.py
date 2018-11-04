127c72018d2f140135a1adafd1ae1d94ee42eac1
Arnaud Aliès <arnaudalies.py@gmail.com>
2018-11-04 09:32:17 -0800

    update quail deploy script

--- quail_build.py
+++ quail_build.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 
-import quail
+import iquail
 
-if not quail.helper.OS_WINDOWS:
+if not iquail.helper.OS_WINDOWS:
     raise AssertionError("This test solution is windows only")
 
-quail.run(
-    solution=quail.SolutionGitHub("xdbot.zip", "https://github.com/mouuff/xdbot"),
-    installer=quail.Installer(
+
+class FrameSelectMiniOrFull(iquail.controller_tkinter.FrameBaseConfigure):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+        self.version_selected = self.add_combobox("Which version would you like to install?",
+                                                  ('English', 'French'))
+
+    def next_pressed(self):
+        print(self.version_selected.get())
+        version = self.version_selected.get().lower()
+        zip = "xdbot.zip" if version == "french" else "xdbot_en.zip"
+        self.manager.config.set("zip_url", zip)
+        self.controller.switch_to_install_frame()
+
+
+iquail.run(
+    solution=iquail.SolutionGitHub(iquail.ConfVar("zip_url"),
+                                   "https://github.com/mouuff/xdbot"),
+    installer=iquail.Installer(
         name='xdbot',
         icon='xdbot.exe',
         binary='xdbot.exe',
@@ -15,9 +31,9 @@ quail.run(
         console=False,
         launch_with_quail=True
     ),
-    builder=quail.builder.Builder(
-        quail.builder.CmdIcon('icon.ico'),
-        quail.builder.CmdNoconsole()
+    builder=iquail.builder.Builder(
+        iquail.builder.CmdIcon('icon.ico'),
+        iquail.builder.CmdNoconsole()
     ),
-    controller=quail.ControllerTkinter()
+    controller=iquail.ControllerTkinter(install_custom_frame=FrameSelectMiniOrFull)
 )
